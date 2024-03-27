2bf1e8d04d895a83f619ab3aa45b2aa2e75d1ce1
Astaroth <105274916+maybeninja@users.noreply.github.com>
2024-03-27 11:10:38 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"BoostTool/Core/Bot"
+	"BoostTool/Core/Discord"
+	"BoostTool/Core/Utils"
+	"time"
+
+	title "github.com/lxi1400/GoTitle"
+)
+
+func main() {
+	config, _ := Utils.LoadConfig() // Ignoring the error for simplicity
+
+	// Continue with the rest of your program
+	Utils.ClearScreen()
+
+	time.Sleep(time.Second * 1)
+	Utils.LogInfo("Logging in.", "", "")
+
+	title.SetTitle("Boost Bot | .gg/astranitros | ")
+	Utils.ClearScreen()
+	Utils.PrintASCII()
+
+	if config.CustomPersonalization.Onliner {
+		Utils.LogInfo("Token Onliner: Enabled", "", "")
+		go Discord.Websocket()
+	} else {
+		Utils.LogInfo("Token Onliner: Disabled", "", "")
+	}
+
+	go Bot.Automation()
+	Bot.StartBot()
+}
